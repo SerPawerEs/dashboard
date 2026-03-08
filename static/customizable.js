241997e95068b83875d6cde0dd5b552d16bc8ba4
SerPawerEs <serpawer.es@gmail.com>
2026-03-08 14:32:39 -0500

    v1

---
+++ static/customizable.js
@@ -0,0 +1,9 @@
+const configbtn = document.getElementById('config')
+const container = document.getElementById('container')
+
+configbtn.addEventListener('click', () => {
+    const pantalla = document.createElement('div')
+    container.appendChild('pantalla')
+    pantalla.className = 'config-container'
+    pantalla.style.display = flex
+})
