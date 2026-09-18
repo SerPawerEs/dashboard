9e182a79ebf96cbf9590350a2a0ba4b395dae71b
SerPawerEs <serpawer.es@gmail.com>
2026-09-18 15:30:26 -0500

    update

--- static/customizable.js
+++ static/customizable.js
@@ -1,7 +1,7 @@
 const configbtn = document.getElementById('configbtn')
 const configdisplay = document.getElementById('configdisplay')
 
-const version = '1.2.4'
+const version = '1.2.5'
 const versionin = document.getElementById('version')
 
 document.addEventListener('DOMContentLoaded', () => {
